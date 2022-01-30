cb24677c09521b6534351704671909c6aac6d8b4
tae100k <tkim3@oswego.edu>
2022-01-30 23:02:00 +0900

    add: 게시글 리스트 기능 추가 [#1]

---
+++ project/src/data/data.js
@@ -0,0 +1,25 @@
+export const items = [
+  {
+    id: 0,
+    title: "나뭇잎 동전지갑",
+    date: "2020.12.04",
+    desc: "코바늘로 제작한 나뭇잎 동전지갑입니다.",
+    src: "./images/leaf-wallet.jpg",
+  },
+  {
+    id: 1,
+    title: "베리베리 손모아장갑",
+    date: "2020.12.19",
+    desc: "대바늘 제작한 나뭇잎 손모아장갑입니다.",
+    src: "./images/berryGlove.jpg",
+  },
+  {
+    id: 2,
+    title: "베리베리 쁘띠 목도리",
+    date: "2020.12.23",
+    desc: "대바늘 제작한 목도리입니다.",
+    src: "./images/berryNeckWarmer.jpg",
+  },
+];
+
+export default items;
